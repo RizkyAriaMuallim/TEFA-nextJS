4fb7767a4fff20377ba64676429d780e68945695
Rizky Aria Mu'allim <74603462+RizkyAriaMuallim@users.noreply.github.com>
2023-05-30 03:11:09 +0700

    combine css with tailwind

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,11 @@
 const NavBar = () => {
     return (
         <>
-            <div className="w-full">
-                <ul className="font-semibold inline-block text-center w-full bg-emerald-200 px-20 py-0 mx-auto my-30">
-                    <li className="inline-block p-3">Amati.</li>
-                    <li className="inline-block p-3">Tiru.</li>
-                    <li className="inline-block p-3">Modifikasi.</li>
+            <div className="navbar">
+                <ul className="navbar-ul">
+                    <li className="navbar-li">Amati.</li>
+                    <li className="navbar-li">Tiru.</li>
+                    <li className="navbar-li">Modifikasi.</li>
                 </ul>
             </div>
         </>
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,17 +2,17 @@ import Link from 'next/link'
 import Card from './card';
 const SideBar = ({component}) => {
     return (
-        <div className="grid grid-cols-testTemplate h-full">
+        <div className="sidebar">
             {/* sidebar */}
             <div className=" bg-gray-950">
-            <ul className='text-neutral-100 font-bold m-4'>
-                <li className='rounded-lg border-2 p-2 border-blue-300 m-1 block'>
+            <ul className='sidebar-ul'>
+                <li className='sidebar-li'>
                     <Link href="/">Root</Link>
                 </li>
-                <li className='rounded-lg border-2 p-2 border-blue-300 m-1 block'>
+                <li className='sidebar-li'>
                     <Link href="/about">About</Link>
                 </li>
-                <li className='rounded-lg border-2 p-2 border-blue-300 m-1 block'>
+                <li className='sidebar-li'>
                     <Link href="/information">Information</Link>
                 </li>
             </ul>
